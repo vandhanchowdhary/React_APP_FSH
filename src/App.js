c845f799c137dc3bfd97ecd2f8a7b7f7b39b568c
Kavuri Vandhan Murali <vandhanchowdhary@gmail.com>
2024-04-14 13:58:49 +0530

    node dependencies updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './style.css'; // Import your CSS file
+import './style.css';
 import Navbar from './Navbar';
 import Home from './Home';
 import About from './About';
